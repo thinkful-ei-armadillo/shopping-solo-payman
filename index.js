247a0c0f6a0d151ec37d7ef58e2de5cc25cbbe8c
Payman Kossari <Paymankossari@gmail.com>
2019-01-19 16:48:06 -0500

    added edit and filtering functionality

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 
 const STORE = {
   items: [],
-  hideChecked: false
+  hideChecked: false,
+  queryString: ''
 };
 
 function generateItemElement(item) {
@@ -11,12 +12,18 @@ function generateItemElement(item) {
       <span class="shopping-item js-shopping-item ${
         item.checked ? 'shopping-item__checked' : ''
       }">${item.name}</span>
+      <input type="text" value="${
+        item.name
+      }" class="hidden shopping-item-edit-name js-shopping-item-edit-name">
       <div class="shopping-item-controls">
         <button class="shopping-item-toggle js-item-toggle">
             <span class="button-label">check</span>
         </button>
         <button class="shopping-item-delete js-item-delete">
             <span class="button-label">delete</span>
+        </button>
+        <button class="shopping-item-edit js-item-edit">
+            <span class="button-label">edit</span>
         </button>
       </div>
     </li>`;
@@ -27,6 +34,12 @@ function generateShoppingItemsString(shoppingList) {
   if (STORE.hideChecked) {
     items = items.filter(function(element) {
       return element.checked === false;
+    });
+  }
+
+  if (STORE.queryString) {
+    items = items.filter(function(element) {
+      return element.name.includes(STORE.queryString);
     });
   }
 
@@ -46,14 +59,8 @@ function addItemToShoppingList(itemName) {
   STORE.items.push({ name: itemName, checked: false, id: cuid() });
 }
 
-function handleNewItemSubmit() {
-  $('#js-shopping-list-form').submit(function(event) {
-    event.preventDefault();
-    const newItemName = $('.js-shopping-list-entry').val();
-    $('.js-shopping-list-entry').val('');
-    addItemToShoppingList(newItemName);
-    renderShoppingList();
-  });
+function addQueryStringFilter(val) {
+  STORE.queryString = val;
 }
 
 function toggleCheckedForListItem(itemIndex) {
@@ -83,19 +90,53 @@ function toggleHideChecked() {
   STORE.hideChecked = !STORE.hideChecked;
 }
 
+function editItemName(newName, id) {
+  let element = STORE.items.find(i => i.id === id);
+  element.name = newName;
+}
+
+function handleItemEditClicked() {
+  $('.js-shopping-list').on('click', '.js-item-edit', event => {
+    let $el = $(event.currentTarget).closest('li');
+    $el.find('.shopping-item').toggleClass('hidden');
+    $el.find('.shopping-item-edit-name').toggleClass('hidden');
+  });
+}
+
+function handleEditingItemName() {
+  $('.js-shopping-list').on('keyup', '.js-shopping-item-edit-name', function(
+    event
+  ) {
+    if (event.keyCode === 13) {
+      let $el = $(event.currentTarget);
+      let newName = $el.val();
+      let id = $el.closest('li').data('item-index');
+      editItemName(newName, id);
+      renderShoppingList();
+    }
+  });
+}
+
+function handleNewItemSubmit() {
+  $('#js-shopping-list-form').submit(function(event) {
+    event.preventDefault();
+    const newItemName = $('.js-shopping-list-entry').val();
+    $('.js-shopping-list-entry').val('');
+    addItemToShoppingList(newItemName);
+    renderShoppingList();
+  });
+}
+
 function handleItemCheckClicked() {
-  $('.js-shopping-list').on('click', `.js-item-toggle`, event => {
-    console.log('`handleItemCheckClicked` ran');
-    debugger;
+  $('.js-shopping-list').on('click', '.js-item-toggle', event => {
     const itemIndex = getItemIndexFromElement(event.currentTarget);
-    debugger;
     toggleCheckedForListItem(itemIndex);
     renderShoppingList();
   });
 }
 
 function handleDeleteItemClicked() {
-  $('.js-shopping-list').on('click', `.js-item-delete`, event => {
+  $('.js-shopping-list').on('click', '.js-item-delete', event => {
     const itemIndex = getItemIndexFromElement(event.currentTarget);
     deleteListItem(itemIndex);
     renderShoppingList();
@@ -108,6 +149,14 @@ function handleClickCheckedFilter() {
     renderShoppingList();
   });
 }
+
+function handleQueryStringFilter() {
+  $('.js-query-search').keyup(function(event) {
+    const queryString = $('.js-query-search').val();
+    addQueryStringFilter(queryString);
+    renderShoppingList();
+  });
+}
 // this function will be our callback when the page loads. it's responsible for
 // initially rendering the shopping list, and activating our individual functions
 // that handle new item submission and user clicks on the "check" and "delete" buttons
@@ -117,7 +166,10 @@ function handleShoppingList() {
   handleNewItemSubmit();
   handleItemCheckClicked();
   handleDeleteItemClicked();
+  handleItemEditClicked();
   handleClickCheckedFilter();
+  handleQueryStringFilter();
+  handleEditingItemName();
 }
 
 // when the page loads, call `handleShoppingList`
